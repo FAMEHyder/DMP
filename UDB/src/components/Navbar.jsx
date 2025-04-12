ca5b6b54e74a852c7a54260972cc9f9e1bb3a9e0
Fame hyder <famehyder9999@gmail.com>
2025-04-12 12:28:17 -0700

    UDB

--- UDB/src/components/Navbar.jsx
+++ UDB/src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
     { label: 'CONTACT US', path: '/contact' },
     { label: 'ABOUT US', path: '/about' }
   ];
-// it is also an
+// it is also
   const platformItems = [
     { label: 'Facebook', path: '/facebook' },
     { label: 'Twitter', path: '/twitter' },
