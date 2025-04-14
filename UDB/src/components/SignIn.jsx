60bbaba264c3cf0d0dc20e16bf1c0e4537859f92
Fame hyder <famehyder9999@gmail.com>
2025-04-14 03:13:02 -0700

    UDB

--- UDB/src/components/SignIn.jsx
+++ UDB/src/components/SignIn.jsx
@@ -7,7 +7,9 @@ import {useNavigate} from 'react-router-dom',
 // This is the functional component of the react 
 const SignIn = () => {
     // logic of the code goes here 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [userName , setuserName ] = useState('');
+    const [Email,setEmail] = useState('');
     const [error, setError] = useState('');
     return (
         // style of the code goes here 
